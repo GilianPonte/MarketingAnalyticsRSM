9efb99b0e4abd02435307a0cc753f09f15c82039
Gilian <gilianponte@live.nl>
2025-03-04 16:36:57 +0100

    Update data.R

--- R/data.R
+++ R/data.R
@@ -1,16 +1,15 @@
 #' Conjoint Analysis Dataset
 #'
-#' This dataset is used for conjoint analysis in marketing analytics. 
-#' It contains information on consumer choices based on various product attributes.
+#' A dataset used for conjoint analysis in marketing.
 #'
 #' @format A data frame with multiple rows and the following columns:
 #' \describe{
 #'   \item{person_id}{Unique identifier for each respondent}
 #'   \item{choice_id}{Choice set identifier}
-#'   \item{urban}{Binary indicator: 1 if the design is urban, -1 if not}
-#'   \item{futuristic}{Binary indicator: 1 if the design is futuristic, -1 if not}
-#'   \item{stylish}{Binary indicator: 1 if the design is stylish, -1 if not}
-#'   \item{retro}{Binary indicator: 1 if the design is retro, -1 if not}
+#'   \item{urban}{Binary indicator: 1 if urban, -1 otherwise}
+#'   \item{futuristic}{Binary indicator: 1 if futuristic, -1 otherwise}
+#'   \item{stylish}{Binary indicator: 1 if stylish, -1 otherwise}
+#'   \item{retro}{Binary indicator: 1 if retro, -1 otherwise}
 #'   \item{exceeds}{Binary indicator: 1 if safety exceeds EU standards, 0 otherwise}
 #'   \item{log.price}{Log-transformed price of the product}
 #'   \item{age}{Standardized age of the respondent}
