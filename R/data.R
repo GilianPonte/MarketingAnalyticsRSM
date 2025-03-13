fd0c8cb8ef566befc1bf9acf930f53d0a186a284
Gilian <gilianponte@live.nl>
2025-03-13 10:18:15 +0100

    Update data.R

--- R/data.R
+++ R/data.R
@@ -1,1 +1,6 @@
-
+download_rfm_data <- function() {
+    url <- "https://raw.githubusercontent.com/GilianPonte/MarketingAnalyticsRSM/main/data/rfm.rda"
+    destfile <- tempfile(fileext = ".rda")
+    download.file(url, destfile, mode = "wb")
+    load(destfile, envir = .GlobalEnv)
+}
