7477bbd9458b58a8e84d791a696f6aff5af18d90
Gilian <gilianponte@live.nl>
2025-03-13 10:58:41 +0100

    Update data.R

--- R/data.R
+++ R/data.R
@@ -4,3 +4,14 @@ download_rfm_data <- function() {
     download.file(url, destfile, mode = "wb")
     load(destfile, envir = .GlobalEnv)
 }
+#' @export
+rfm_from_transactions <- function(transactions) {
+  transactions %>%
+    dplyr::mutate(transaction = revenue > 0) %>%
+    dplyr::group_by(ID) %>%
+    dplyr::summarise(x = sum(transaction),
+              t.x = max(week * transaction),
+              m.x = sum(revenue * transaction) / x,
+              n.cal = 52) %>%
+    dplyr::mutate(m.x = dplyr::coalesce(m.x, 0))
+}
